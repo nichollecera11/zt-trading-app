869721a429681115acc785cda32da46fe4459295
Nichol lecera <nichollecera11@gmail.com>
2026-08-04 13:56:31 +0800

    fix: made database connection variables bulletproof

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,12 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: 24078,
-  user: 'avnadmin',
-  password: process.env.DB_PASSWORD,
-  database: 'defaultdb',
+  // This tells Next.js to check for MYSQL_HOST first, and if it can't find it, use DB_HOST
+  host: process.env.MYSQL_HOST || process.env.DB_HOST,
+  port: process.env.MYSQL_PORT || 24078,
+  user: process.env.MYSQL_USER || 'avnadmin',
+  password: process.env.MYSQL_PASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQL_DATABASE || 'defaultdb',
   ssl: {
     rejectUnauthorized: false
   }
